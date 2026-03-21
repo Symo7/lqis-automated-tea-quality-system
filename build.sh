8c01348daa1ef6af13258e168b26be004cbd6484
Symo7 <161832634+Symo7@users.noreply.github.com>
2026-03-21 09:21:52 +0300

    chore: automate demo data seeding during build phase to bypass shell paywall

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 cd lqis_project
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
+
+# Run the seed data command during the build phase so we don't need the paid Shell
+python manage.py seed_demo_data
