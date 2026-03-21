fe21a8e9d5178e9ee09d5cc74c0253480fc7695c
Symo7 <161832634+Symo7@users.noreply.github.com>
2026-03-21 07:19:05 +0300

    chore: added start.sh to run background workers and web server on a single free container constraint

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# start.sh
+# Run background worker and the web server in the exact same container to avoid paying for two machines.
+
+echo "Starting Django Q Cluster in the background..."
+python lqis_project/manage.py qcluster &
+
+echo "Starting Gunicorn Web Server..."
+cd lqis_project && gunicorn lqis_project.wsgi:application --bind 0.0.0.0:$PORT
